cec3694d20828c90f08aa4b2b24811b957f1cb7a
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-09-03 12:07:52 +0530

    trying to fix image upload problem in firebase

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,18 +4,19 @@ const router = express.Router();
 const User = require("../models/userModel");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const multer  = require('multer')
-const storage = multer.memoryStorage(); // Store the uploaded image in memory
-const upload = multer({ storage });
+const multer = require('multer')
+const multerStorage = multer.memoryStorage(); // Store the uploaded image in memory
+const upload = multer({ multerStorage });
 const saltRounds = 10;
 const { initializeApp } = require('firebase/app'); // require firebase
 const { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } = require("firebase/storage"); // getting required services from firestore
 const firebaseConfig = require("../firebase/firebaseConfig");
+const randomString = require("randomstring");
 
 // --- firebase APP --
 const fireBaseApp = initializeApp(firebaseConfig);
-const fireBaseStorage = getStorage(fireBaseApp); //points to root directory 
-const profileImagesRef = ref(fireBaseStorage,"images/profileImages")
+const storage = getStorage(fireBaseApp); //points to root directory 
+const profileImagesRef = ref(storage, "/images/profileImages")
 // for 41 it will be images/profileImages/41/image-url
 
 router.post("/createUser",
@@ -51,16 +52,69 @@ router.post("/createUser",
                },
                fieldOfInterest, tag, gradCourse, rollNum
             })
-            await newUser.save();
-            // --- Create JWT token ---
-            const data = { userId: newUser._id };
-            const token = jwt.sign(data, process.env.JWT_SECRET_CODE)
-            res.json({
-               success: true,
-               message: "user created",
-               user: newUser,
-               token
-            })
+            // ---------- save file here -------
+             // --- Create JWT token ---
+             const data = { userId: newUser._id };
+             const token = jwt.sign(data, process.env.JWT_SECRET_CODE)
+            let userProfileUrl;
+            if (req.file) {
+               // profile pic 
+               const fileType = req.file.mimetype;
+               const fileOriginalName = req.file.originalname;
+               const bufferData = req.file.buffer;
+               const now = new Date();
+               const dateStamp = now.toISOString();
+               const randString = randomString.generate({ length: 12, charset: "alphanumeric" })
+               const docGivenName = fileOriginalName + dateStamp + randString;
+               const metaData = {
+                  contentType: fileType
+               }
+               let uploadTask;
+               if (fileType === "image/jpeg" || fileType === "image/png") {
+                  const uploadProfilePicRef = ref(profileImagesRef, `/${docGivenName}`);
+                  uploadTask = uploadBytes(uploadProfilePicRef, bufferData, metaData);
+                  uploadTask.then((snapshot) => {
+                     getDownloadURL(snapshot.ref).then(async (downloadURL) => {
+                         userProfileUrl = downloadURL;
+                         console.log(downloadURL);
+                        // save profile picture url to users collection
+                        // newUser.profilePic.givenName = docGivenName ;
+                        // newUser.profilePic.url = downloadURL ;
+                         newUser.profilePic = {
+                           givenName : docGivenName,
+                           url : downloadURL
+                        }
+                        console.log(newUser);
+                        await newUser.save();
+                        res.json({
+                           success: true,
+                           message: "user created",
+                           user: newUser,
+                           token
+                        })
+                     })
+                  }).catch(async (error)=>{
+                     console.log(error);
+                     await newUser.save();
+                     res.json({
+                        success: true,
+                        message: "Firebase error came",
+                        user: newUser,
+                        token
+                     })
+                  })
+               }
+               // profile pic upload ends
+            }else{
+                //console.log(userProfileUrl);
+               await newUser.save();
+               res.json({
+                  success: true,
+                  message: "user created",
+                  user: newUser,
+                  token
+               })
+            }
          }
       } catch (error) {
          console.log(error);
@@ -70,7 +124,7 @@ router.post("/createUser",
             error
          })
       }
-});
+   });
 
 
 
